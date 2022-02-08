47383a14e8b4ba526211a17e4e278564917e2eee
Haritha1216 <94508568+Haritha1216@users.noreply.github.com>
2022-02-08 21:54:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Image from './images/liz.jpg';
+//import Image from './images/liz.jpg';
 import "./styles.css";
 import Assignment from './Assignment.js';
 
